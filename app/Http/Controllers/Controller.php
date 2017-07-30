f09130734fc48462a45faa688d904f46dd14952b
Yohann Payrot <yohann.payrot@fakedomain.com>
2017-07-30 23:43:35 +0200

    Checkstyle Controller file

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Slim\Container;
 
 class Controller {
 
+    /**
+     * @var Container
+     */
     private $container;
 
-    public function __construct($container) {
+    public function __construct(Container $container) {
         $this->container = $container;
     }
 
